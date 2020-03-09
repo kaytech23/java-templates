5b27f5759c57ee524563f5207eb5994259440ce6
Kay <kaytech23@gmail.com>
2020-03-09 11:07:43 +0100

    Adds spring security with permitAll()

---
+++ src/main/java/com/test/app/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.test.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// permitAll not allowing anonymous access:
+// https://stackoverflow.com/questions/24696717/spring-security-permitall-not-allowing-anonymous-access
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
